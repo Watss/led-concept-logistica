ea6b53fff4f0c09576e1d9eb0c35fe3cb806cfb1
abel <eliasvalenzuela165@gmail.com>
2021-07-27 20:49:58 -0400

    feat : add seed product and index template

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
 
         <div class="mb-3">
-            <h1 class="h3 d-inline align-middle">Cards</h1>
+            <h1 class="h3 d-inline align-middle">Cards  </h1>
             <a class="badge bg-dark text-white ms-2" href="upgrade-to-pro.html">
 Get more card examples
 </a>
--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -1,77 +1,87 @@
 <x-app-layout>
-    <div class="rounded bg-white border border-gray-300 mt-6">
+  <div class="mb-3">
+    <h1 class="h3 d-inline align-middle">Productos </h1>
+    <!-- <a class="badge bg-dark text-white ms-2" href="upgrade-to-pro.html">
+      Get more card examples
+    </a> -->
+  </div>
+  <div class="row">
+    <div class="col-12 col-lg-12 col-xxl-12 d-flex">
+      <div class="card flex-fill p-4">
 
-        <!-- header -->
-        <div class="border-b p-6 flex flex-row justify-between">
-            <h1 class="h6">Productos</h1>
-        </div>
-        <!-- end header -->
+        <table class="table table-hover my-0">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th class="d-none d-xl-table-cell">Start Date</th>
+              <th class="d-none d-xl-table-cell">End Date</th>
+              <th>Status</th>
+              <th class="d-none d-md-table-cell">Assignee</th>
+            </tr>
+          </thead>
+          <tbody>
 
-        <div class="flex flex-col">
-            <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-              <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-                <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                  <table class="min-w-full divide-y divide-gray-200">
-                    <thead class="bg-gray-50">
-                      <tr>
-                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                          Name
-                        </th>
-                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                          Title
-                        </th>
-                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                          Status
-                        </th>
-                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                          Role
-                        </th>
-                        <th scope="col" class="relative px-6 py-3">
-                          <span class="sr-only">Edit</span>
-                        </th>
-                      </tr>
-                    </thead>
-                    <tbody class="bg-white divide-y divide-gray-200">
-                      <tr>
-                        <td class="px-6 py-4 whitespace-nowrap">
-                          <div class="flex items-center">
-                            <div class="flex-shrink-0 h-10 w-10">
-                              <img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60" alt="">
-                            </div>
-                            <div class="ml-4">
-                              <div class="text-sm font-medium text-gray-900">
-                                Jane Cooper
-                              </div>
-                              <div class="text-sm text-gray-500">
-                                jane.cooper@example.com
-                              </div>
-                            </div>
-                          </div>
-                        </td>
-                        <td class="px-6 py-4 whitespace-nowrap">
-                          <div class="text-sm text-gray-900">Regional Paradigm Technician</div>
-                          <div class="text-sm text-gray-500">Optimization</div>
-                        </td>
-                        <td class="px-6 py-4 whitespace-nowrap">
-                          <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                            Active
-                          </span>
-                        </td>
-                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                          Admin
-                        </td>
-                        <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                          <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
-                        </td>
-                      </tr>
-
-                      <!-- More people... -->
-                    </tbody>
-                  </table>
-                </div>
-              </div>
-            </div>
-          </div>
-
+            
+            <tr>
+              <td>Project Apollo</td>
+              <td class="d-none d-xl-table-cell">01/01/2021</td>
+              <td class="d-none d-xl-table-cell">31/06/2021</td>
+              <td><span class="badge bg-success">Done</span></td>
+              <td class="d-none d-md-table-cell">Vanessa Tucker</td>
+            </tr>
+            <tr>
+              <td>Project Fireball</td>
+              <td class="d-none d-xl-table-cell">01/01/2021</td>
+              <td class="d-none d-xl-table-cell">31/06/2021</td>
+              <td><span class="badge bg-danger">Cancelled</span></td>
+              <td class="d-none d-md-table-cell">William Harris</td>
+            </tr>
+            <tr>
+              <td>Project Hades</td>
+              <td class="d-none d-xl-table-cell">01/01/2021</td>
+              <td class="d-none d-xl-table-cell">31/06/2021</td>
+              <td><span class="badge bg-success">Done</span></td>
+              <td class="d-none d-md-table-cell">Sharon Lessman</td>
+            </tr>
+            <tr>
+              <td>Project Nitro</td>
+              <td class="d-none d-xl-table-cell">01/01/2021</td>
+              <td class="d-none d-xl-table-cell">31/06/2021</td>
+              <td><span class="badge bg-warning">In progress</span></td>
+              <td class="d-none d-md-table-cell">Vanessa Tucker</td>
+            </tr>
+            <tr>
+              <td>Project Phoenix</td>
+              <td class="d-none d-xl-table-cell">01/01/2021</td>
+              <td class="d-none d-xl-table-cell">31/06/2021</td>
+              <td><span class="badge bg-success">Done</span></td>
+              <td class="d-none d-md-table-cell">William Harris</td>
+            </tr>
+            <tr>
+              <td>Project X</td>
+              <td class="d-none d-xl-table-cell">01/01/2021</td>
+              <td class="d-none d-xl-table-cell">31/06/2021</td>
+              <td><span class="badge bg-success">Done</span></td>
+              <td class="d-none d-md-table-cell">Sharon Lessman</td>
+            </tr>
+            <tr>
+              <td>Project Romeo</td>
+              <td class="d-none d-xl-table-cell">01/01/2021</td>
+              <td class="d-none d-xl-table-cell">31/06/2021</td>
+              <td><span class="badge bg-success">Done</span></td>
+              <td class="d-none d-md-table-cell">Christina Mason</td>
+            </tr>
+            <tr>
+              <td>Project Wombat</td>
+              <td class="d-none d-xl-table-cell">01/01/2021</td>
+              <td class="d-none d-xl-table-cell">31/06/2021</td>
+              <td><span class="badge bg-warning">In progress</span></td>
+              <td class="d-none d-md-table-cell">William Harris</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
+
+  </div>
 </x-app-layout>
