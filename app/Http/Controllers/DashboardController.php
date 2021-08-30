5c72ead115e02f2b4378c6a593597919597b31b7
leandro <leandrofernando739@gmail.com>
2021-08-29 23:35:47 -0400

    fix: info dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,15 +5,21 @@ namespace App\Http\Controllers;
 use App\Models\Budget;
 use App\Models\Client;
 use App\Models\Product;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index()
     {
+       $now= Carbon::createFromFormat('Y-m-d H:i:s', now()->toDateString(). '00:00:00')->toDateTimeString();
        $products= Product::active()->get()->count();
        $clients= Client::all()->count();
-       $budgets= Budget::all()->count();
-        return view('dashboard',['products'=>$products,'clients'=>$clients,'budgets'=>$budgets]);
+       $budgets= Budget::whereDate('created_at',$now)->get();
+
+       $budgetAcept=$budgets->where('budget_statuses_id',1)->count();
+       $budgetRejected=$budgets->where('budget_statuses_id',2)->count();
+
+        return view('dashboard',['products'=>$products,'clients'=>$clients,'budgetAcept'=>$budgetAcept,'budgetRejected'=>$budgetRejected]);
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
 
     <div class="mb-3">
-        <h1 class="h3 d-inline align-middle">Dashboard</h1>
+        <h1 class="h3 d-inline align-middle">Dashboard (informe diario)</h1>
         <a class="badge bg-dark text-white ms-2" href="upgrade-to-pro.html">
             Get more card examples
         </a>
@@ -14,7 +14,7 @@
                         <div class="card-body">
                             <div class="row">
                                 <div class="col mt-0">
-                                    <h5 class="card-title">Total cotizaciones</h5>
+                                    <h5 class="card-title">Total cotizaciones Aceptadas</h5>
                                 </div>
 
                                 <div class="col-auto">
@@ -31,7 +31,7 @@
                                     </div>
                                 </div>
                             </div>
-                            <h1 class="mt-1 mb-3">{{$budgets}}</h1>
+                            <h1 class="mt-1 mb-3">{{$budgetAcept}}</h1>
                         </div>
                     </div>
                 </div>
@@ -40,7 +40,7 @@
                         <div class="card-body">
                             <div class="row">
                                 <div class="col mt-0">
-                                    <h5 class="card-title">Total productos</h5>
+                                    <h5 class="card-title">Total cotizaciones rechazadas</h5>
                                 </div>
 
                                 <div class="col-auto">
@@ -48,16 +48,16 @@
                                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
                                             viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
                                             stroke-linecap="round" stroke-linejoin="round"
-                                            class="feather feather-truck align-middle">
-                                            <rect x="1" y="3" width="15" height="13"></rect>
-                                            <polygon points="16 8 20 8 23 11 23 16 16 16 16 8"></polygon>
-                                            <circle cx="5.5" cy="18.5" r="2.5"></circle>
-                                            <circle cx="18.5" cy="18.5" r="2.5"></circle>
+                                            class="feather feather-shopping-cart align-middle">
+                                            <circle cx="9" cy="21" r="1"></circle>
+                                            <circle cx="20" cy="21" r="1"></circle>
+                                            <path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6">
+                                            </path>
                                         </svg>
                                     </div>
                                 </div>
                             </div>
-                            <h1 class="mt-1 mb-3">{{$products}}</h1>
+                            <h1 class="mt-1 mb-3">{{$budgetRejected}}</h1>
                         </div>
                     </div>
                 </div>
@@ -98,7 +98,7 @@
                         <div class="card-body">
                             <div class="row">
                                 <div class="col mt-0">
-                                    <h5 class="card-title">Sales</h5>
+                                    <h5 class="card-title">Total Productos</h5>
                                 </div>
 
                                 <div class="col-auto">
@@ -115,7 +115,7 @@
                                     </div>
                                 </div>
                             </div>
-                            <h1 class="mt-1 mb-3">2.382</h1>
+                            <h1 class="mt-1 mb-3">{{$products}}</h1>
                         </div>
                     </div>
                 </div>
