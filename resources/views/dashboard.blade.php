0c819d30eec86634fa78fe6906ed658b94a1aa30
= <eliasvalenzuela165@gmail.com>
2021-10-25 20:49:33 -0300

    fix : remove badge in dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,9 +2,9 @@
 
     <div class="mb-3">
         <h1 class="h3 d-inline align-middle">Dashboard (informe diario)</h1>
-        <a class="badge bg-dark text-white ms-2" href="upgrade-to-pro.html">
+        {{-- <a class="badge bg-dark text-white ms-2" href="upgrade-to-pro.html">
             Get more card examples
-        </a>
+        </a> --}}
     </div>
     <div class="row">
         <div class="col-3 col-md-3">
